fc48d099ce2bbae917df92a987d8ce55e7e8ff20
= <=>
2021-09-25 15:10:11 +0200

    use new team and player component in main page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,6 +4,7 @@ import useSWR from "swr";
 import tw from "twin.macro";
 import { request, gql } from "graphql-request";
 import Team from "../componets/Team";
+import Player from "../componets/Player";
 
 const getTeams = gql`
   query {
@@ -40,7 +41,16 @@ const Home: React.FC = () => {
         <title>Esportliga mini site</title>
       </Head>
       {teamData.teams.map((t) => (
-        <Team key={t.id} name={t.name} players={t.players} />
+        <Team key={t.id} name={t.name}>
+          {t.players.map((player) => (
+            <Player
+              key={player.id}
+              id={player.id}
+              role={player.role}
+              username={player.username}
+            />
+          ))}
+        </Team>
       ))}
     </Container>
   );
