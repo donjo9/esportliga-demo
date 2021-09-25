147291a9da0eb4eaa5759d4aa9d3933c92f452dd
= <=>
2021-09-25 15:09:37 +0200

    refactor team component

--- client/componets/Team.tsx
+++ client/componets/Team.tsx
@@ -1,16 +1,24 @@
-import Player from "./Player";
+import styled from "styled-components";
+import tw from "twin.macro";
 
-const Team: React.FC<{ name: string; players: Array<any> }> = ({
-  name,
-  players,
-}) => {
+const TeamTitle = styled.div`
+  ${tw`p-2 text-xl`}
+`;
+
+const PlayerHeader = styled.h1`
+  ${tw`px-2 text-xl`}
+`;
+const PlayerGrid = styled.div`
+  ${tw`grid grid-cols-1 w-max p-2`}
+`;
+
+const Team: React.FC<{ name: string }> = ({ name, children }) => {
   return (
-    <div>
-      {name}
-      {players.map((p) => (
-        <Player key={p.id} {...p} />
-      ))}
-    </div>
+    <>
+      <TeamTitle>{name}</TeamTitle>
+      <PlayerHeader>Players:</PlayerHeader>
+      <PlayerGrid>{children}</PlayerGrid>
+    </>
   );
 };
 
