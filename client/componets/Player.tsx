d2849af5f42d61e4699153be87d8fb6573bc79ec
= <=>
2021-09-25 15:09:54 +0200

    refactor player component

--- client/componets/Player.tsx
+++ client/componets/Player.tsx
@@ -1,15 +1,26 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+
 type TPlayer = {
   id: string;
   username: string;
   role: string;
 };
 
-const Player: React.FC<TPlayer> = ({ id, username, role }) => {
+const PlayerContainer = styled.div`
+  ${tw`flex flex-row my-1`}
+`;
+
+const UserHeader = styled.h2`
+  ${tw`mr-auto px-2`}
+`;
+const Player: React.FC<TPlayer> = ({ username, children }) => {
   return (
-    <div>
-      {" "}
-      {username} {role}
-    </div>
+    <PlayerContainer>
+      <UserHeader>{username}</UserHeader>
+
+      {children}
+    </PlayerContainer>
   );
 };
 
