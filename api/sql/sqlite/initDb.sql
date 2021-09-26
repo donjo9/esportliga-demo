86d33eca6ddbbb1a38bd2d56c9115243a387244b
Johnni Dybdahl Mortensen <don@jo9.dk>
2021-09-26 16:39:24 +0200

    remove usused statemens from initDb.sql

--- api/sql/sqlite/initDb.sql
+++ api/sql/sqlite/initDb.sql
@@ -33,8 +33,4 @@ CREATE TABLE IF NOT EXISTS team_invitations (
     FOREIGN KEY (playerId) REFERENCES users (id) ON DELETE CASCADE
     FOREIGN KEY (teamId) REFERENCES teams (id) ON DELETE CASCADE
 );
-SELECT * from player_team_realation;
-
-SELECT * from player_team_realation WHERE playerId = 'u2l4UStXbZh4nfjRpcG27' AND teamId = '25O2txdHyWFRQqPbkXJSU';
-
-DELETE FROM team_invitations WHERE TRUE;
+/* 
